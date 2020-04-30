a12a89ede51ea606715fc45cf43d341210b50dd9
sairina <sairina.merinotsui@gmail.com>
2020-04-29 21:35:59 -0700

    added render todo list case

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { ADD_TODO } from '../actions';
+import { RENDER_TODO_LIST } from '../actions';
 
 //define initial state of app
 const initialState = {
@@ -9,6 +10,9 @@ const initialState = {
 //add new todo item to the toDoList
 export default function toDoApp(state = initialState, action) {
   switch (action.type) {
+    case RENDER_TODO_LIST:
+      //when reducer receives action, it will read toDoList payload and update state of app with new list
+      return { ...state, toDoList: action.toDoList };
     case ADD_TODO:
       let newToDoList = [...state.toDoList, { ...action.toDoItem }];
       return { ...state, toDoList: newToDoList };
