2a166a267cc7cf8b165c4341a4cf1f567acbf072
sairina <sairina.merinotsui@gmail.com>
2020-04-29 20:20:37 -0700

    added container to map state in store to prop

---
+++ src/containers/ToDoListContainer.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import ToDoList from '../components/TodoList';
+
+//map state of app to props of todolist component
+//redux will map todolist object on its store to the todolist prop
+//that todolist component uses
+const mapStateToProps = (state) => {
+  return {
+    toDoList: state.toDoList,
+  };
+};
+
+const ToDoListContainer = connect(mapStateToProps)(ToDoList);
+
+export default ToDoListContainer;
