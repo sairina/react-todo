722c1cadf16224794d6d3e9048a8211f3912406a
sairina <sairina.merinotsui@gmail.com>
2020-04-29 21:31:33 -0700

    added load todo list action type and fn

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 export const ADD_TODO = 'ADD_TODO';
+export const LOAD_TODO_LIST = 'LOAD_TODO_LIST'; //makes app load todolist from external server
+export const RENDER_TODO_LIST = 'RENDER_TODO_LIST'; //renders todo list it just loaded
 
 //app handles action ADD_TODO
 //toDoItem = object with 2 properties, id and title
@@ -9,5 +11,12 @@ export function addToDo(title) {
       _id: new Date().getTime(),
       title,
     },
+  };
+}
+
+//defines function to create action with LOAD_TODO_LIST type
+export function loadToDoList() {
+  return {
+    type: LOAD_TODO_LIST,
   };
 }
